3a6ebe3a70efdd20a9dbdde95177da41e7b7b470
DimyLeo <leonardoprimoc@hotmail.com>
2022-12-26 16:31:42 -0300

    Iniciando o Front

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Header />
+    <h1>Teste</h1>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: #2d2d2d;
+  color: white;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>Header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header-head',
+}
+</script>
+
+<style scoped>
+
+</style>
