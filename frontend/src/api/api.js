cd2e3775326b38cb154af1419ab524db7dd66bea
DimyLeo <leonardoprimoc@hotmail.com>
2022-12-29 15:22:17 -0300

    Config variavel do host da api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const PROTOCOL = process.env.APP_API_PROTOCOL || 'http';
-const HOST = process.env.APP_API_HOST || 'localhost';
+const HOST = process.env.APP_API_HOST || 'localhost:3000';
 
 // export const api = axios.create({ baseURL: 'back-cashforce-test.up.railway.app' });
 export const api = axios.create({ 
-  baseURL: `${PROTOCOL}://${HOST}`, 
+  baseURL: `${PROTOCOL}://${HOST}`,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
