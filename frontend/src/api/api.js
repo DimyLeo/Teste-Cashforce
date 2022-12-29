bd4eaec9cab369b3c52d7d91d1e88b0d555bd35a
DimyLeo <leonardoprimoc@hotmail.com>
2022-12-29 15:00:04 -0300

    add configs de deploy

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,7 @@
+// require('dotenv').config();
 import axios from 'axios';
 
-export const api = axios.create({ baseURL: 'https://back-cashforce-test.up.railway.app' });
+const APP_API_HOST = process.env.APP_API_HOST || 'http://localhost:3000';
+
+// export const api = axios.create({ baseURL: 'back-cashforce-test.up.railway.app' });
+export const api = axios.create({ baseURL: APP_API_HOST });
