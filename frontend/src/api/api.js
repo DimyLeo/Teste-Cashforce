dc9ef42cb74cb16ab2fe770dafab02e318dde1a7
DimyLeo <leonardoprimoc@hotmail.com>
2022-12-30 12:11:45 -0300

    config rota api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const PROTOCOL = process.env.APP_API_PROTOCOL || 'http';
-const HOST = process.env.APP_API_HOST || 'localhost:3000';
+// const PROTOCOL = process.env.APP_API_PROTOCOL || 'http';
+// const HOST = process.env.APP_API_HOST || 'localhost:3000';
 
-// export const api = axios.create({ baseURL: 'back-cashforce-test.up.railway.app' });
-export const api = axios.create({ 
-  baseURL: `${PROTOCOL}://${HOST}`,
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  }
-});
+export const api = axios.create({ baseURL: 'https://back-cashforce-test.up.railway.app' });
+// export const api = axios.create({ 
+//   baseURL: `${PROTOCOL}://${HOST}`,
+//   headers: {
+//     'Accept': 'application/json',
+//     'Content-Type': 'application/json'
+//   }
+// });
